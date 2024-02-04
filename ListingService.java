09a53051ec6ce714e1e8e187c722e337fedf7e6f
Jared Chapman <thejaredchapman@gmail.com>
2024-02-03 19:43:41 -0600

    remove keys

--- ListingService.java
+++ ListingService.java
@@ -12,10 +12,10 @@ public class ListingService {
 
     HttpRequest request = HttpRequest.newBuilder()
 		.uri(URI.create("https://zillow69.p.rapidapi.com/agent/soldListings?zuid=%3CREQUIRED%3E"))
-		.header("X-RapidAPI-Key", "9202cb6d35msh267f818513d4c85p1364fdjsn3cbef17f0867")
-		.header("X-RapidAPI-Host", "zillow69.p.rapidapi.com")
+		.header("X-RapidAPI-Key", "#")
+		.header("X-RapidAPI-Host", "#")
 		.method("GET", HttpRequest.BodyPublishers.noBody())
 		.build();
-HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-System.out.println(response.body());
+    HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+    System.out.println(response.body());
 }
