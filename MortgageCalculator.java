e1a43461948169178dd32031f441dff475c5f9cc
Jared Chapman <deraj@google.com>
2024-04-23 19:36:45 -0500

    update value tracker

--- MortgageCalculator.java
+++ MortgageCalculator.java
@@ -1,10 +1,9 @@
 import java.text.NumberFormat;
-import java.util.HashMap;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class MortgageCalculator {
-
 
 	public static class ValueTracker{
 	
@@ -12,9 +11,11 @@ public class MortgageCalculator {
 		public double principalAtCurrentMonth;
 		public double totalInterest;
 		public double totalPrincipal;
+		public String date;
 
-		public ValueTracker(double interestAtCurrentMonth, double principalAtCurrentMonth, 
+		public ValueTracker(String date, double interestAtCurrentMonth, double principalAtCurrentMonth, 
 				double totalInterest, double totalPrincipal) {
+			this.date = date;
 			this.interestAtCurrentMonth = interestAtCurrentMonth;
 			this.principalAtCurrentMonth = principalAtCurrentMonth;
 			this.totalInterest = totalInterest;
@@ -26,21 +27,20 @@ public class MortgageCalculator {
 		}
 
 		public String toString(){
-			return String.format("This will return the interest at current month %f this is the %f, this is the %f, and this is the %f\n", interestAtCurrentMonth, principalAtCurrentMonth, totalInterest, totalPrincipal);
-			
+			return String.format("This will return the interest at current month %f this is the %f, this is the %f, and this is the %f\n", interestAtCurrentMonth, principalAtCurrentMonth, totalInterest, totalPrincipal);	
 		}
 	}
 		 
 	private static final int MONTHS_IN_A_YEAR = 12;
-	private static final int DEFAULT_TERM = 15;
-	private static final int TOTAL_PAYMENTS = 180;
+	private static final int DEFAULT_TERM = 30;
+	private static final int TOTAL_PAYMENTS = 360;
 
 	public static void main(String[] args) {
 
+		Calendar date = Calendar.getInstance();
+
+		ValueTracker valueT = new ValueTracker(date.getTime().toString(), DEFAULT_TERM, TOTAL_PAYMENTS, MONTHS_IN_A_YEAR, DEFAULT_TERM); 
 		
-
-		ValueTracker valueT = new ValueTracker(DEFAULT_TERM, TOTAL_PAYMENTS, MONTHS_IN_A_YEAR, DEFAULT_TERM); 
-
 		valueT.getInterestAtCurrentMonth();
 		System.out.println(valueT.toString());
 
@@ -57,8 +57,7 @@ public class MortgageCalculator {
         scanner.close();
 
         double principal = listingPrice - downPayment;
-		
-    
+		   
 		double P = principal;
 		double t = DEFAULT_TERM;
 		double n = MONTHS_IN_A_YEAR;
@@ -78,21 +77,19 @@ public class MortgageCalculator {
 			totalInterest += monthlyInterest;
 			currentPrincipal -= monthlyPrincipal;
 	
-			ValueTracker ve = new ValueTracker(monthlyInterest, monthlyPrincipal, totalInterest, currentPrincipal);
+			ValueTracker ve = new ValueTracker(date.getTime().toString(), monthlyInterest, monthlyPrincipal, totalInterest, currentPrincipal);
 			paymentsMade.add(ve);
+			
 			String output = ve.toString();
+			System.out.println(output);
 		}
 		
-		
-
-
 		// PRINCIPAL - MORTGAGE PAYMENT WITHOUT INTEREST
 
 		System.out.println("Your principal is " + NumberFormat.getCurrencyInstance().format(principal));
 		System.out.println("Monthly payment: "+ NumberFormat.getCurrencyInstance().format(monthlyPayment));
 		System.out.println("Your annual interest rate is " + annualInterestRate);
 		System.out.println("Total payback amount or Amoritzation: " + NumberFormat.getCurrencyInstance().format(totalPaybackAmount));
-		System.out.println(paymentsMade);
+		
 	}
-
 }
